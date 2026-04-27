b3fe4d79726e937a95b817bdaa894decff36345e
mokev10 <mokev10@outlook.com>
2026-04-27 14:46:32 +0200

    Update index.py

--- index.py
+++ index.py
@@ -7,8 +7,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="Tynorah — Sign In",
-    page_icon="https://https://img.icons8.com/external-line-zulfa-mahendra/48/external-robot-future-technology-line-zulfa-mahendra.png",
+    page_title="TYNORA SIGN IN",
+    page_icon="https://img.icons8.com/external-line-zulfa-mahendra/48/external-robot-future-technology-line-zulfa-mahendra.png",
     layout="wide",
     initial_sidebar_state="expanded",
 )
